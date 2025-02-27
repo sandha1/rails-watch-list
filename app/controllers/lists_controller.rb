eacd6ca06e49a15b696e6533bc0fbfa525cb9f2c
Sandrine Halbronn sandhales@gmail.com <sandhales@gmail.com>
2025-02-27 15:22:56 +0100

    Added my controllers and views

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,29 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params_list)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmarks = Bookmark.all
+  end
+
+  private
+
+  def params_list
+    params.require(:list).permit(:name)
+  end
+end
